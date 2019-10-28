1afa27f64dc2115fca672c8f53b959c3cd017c5d
Stefanos Vichas <stefanos@vichas.org>
2019-10-28 21:46:32 +0200

    Adding text

--- wave.js
+++ wave.js
@@ -1,21 +1,29 @@
 
 
 var canvas = document.querySelector(".wavecanvas"),
-    context = canvas.getContext("2d");
+    context = canvas.getContext("2d"),
+    sinDelta = 0,
+    waveLine = 20,
+    waveTextLeft = window.innerWidth;
 
 canvas.width = window.innerWidth;
 canvas.height = 500;
-  
-    
+
+var waveTextElements = document.querySelectorAll(".wavecanvas_txt");
+
+for (var i=0;i<waveTextElements.length;i++) {
+    waveTextElements[i].style.left = waveTextLeft;
+    waveTextLeft+=350;
+}
+
 // tick
-function drawWave(delta) {
-    requestAnimationFrame(drawWave);
+function waveDraw() {
     canvas.width = canvas.width;
     
-    var randomLeft = Math.abs(Math.pow( Math.sin(delta/1000), 2 )) * 100,
-        randomRight = Math.abs(Math.pow( Math.sin((delta/1000) + 10), 2 )) * 100,
-        randomLeftConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+2), 2 )) * 100,
-        randomRightConstraint = Math.abs(Math.pow( Math.sin((delta/1000)+1), 2)) * 100;
+    var randomLeft              = Math.abs(Math.pow(Math.sin(sinDelta/1000), 2)) * 100,
+        randomRight             = Math.abs(Math.pow(Math.sin((sinDelta/1000) + 10), 2)) * 100,
+        randomLeftConstraint    = Math.abs(Math.pow(Math.sin((sinDelta/1000)+2), 2)) * 100,
+        randomRightConstraint   = Math.abs(Math.pow(Math.sin((sinDelta/1000)+1), 2)) * 100;
     
     context.fillStyle = "#005bab";
     context.beginPath();
@@ -29,15 +37,31 @@ function drawWave(delta) {
 
     context.fillStyle = "#0074D9";
     context.beginPath();
-    context.moveTo(0, randomLeft+20);
-    context.bezierCurveTo(canvas.width / 3, randomLeftConstraint+20, canvas.width / 3 * 2, randomRightConstraint+20, canvas.width, randomRight+20);
-    context.lineTo(canvas.width , canvas.height+20);
-    context.lineTo(0, canvas.height+20);
-    context.lineTo(0, randomLeft+20);
+    context.moveTo(0, randomLeft+waveLine);
+    context.bezierCurveTo(canvas.width / 3, randomLeftConstraint+waveLine,
+                          canvas.width / 3 * 2, randomRightConstraint+waveLine,
+                          canvas.width, randomRight+waveLine);
+    context.lineTo(canvas.width , canvas.height+waveLine);
+    context.lineTo(0, canvas.height+waveLine);
+    context.lineTo(0, randomLeft+waveLine);
     context.closePath();
     context.fill();
 
+    for (var i=0;i<waveTextElements.length;i++) {
+        var cLeft = parseInt(waveTextElements[i].style.left);
+        waveTextElements[i].style.left = cLeft - 1;
+        if (cLeft + waveTextElements[i].offsetWidth < 0) {
+            waveTextElements[i].style.left = window.innerWidth;
+        }
+
+        // calc top
+        
+        
+
+
+    }
+    
+    sinDelta += 5;
 }
 
-
-requestAnimationFrame(drawWave);
+var tickTimer = setInterval(waveDraw, 1000/60);
